0ef1fe8e978502603e088abe556b56e10ed3a33b
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-12-22 07:11:45 +0200

    implement show comments feature

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,13 +1,42 @@
 import React, { useEffect, useState } from "react";
 import moment from "moment";
 import parse from "html-react-parser";
+import { getComments } from "../services";
 
 const Comments = ({ slug }) => {
   const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments(slug).then((res) => {
+      setComments(res);
+    });
+  }, []);
+
   return (
-    <div>
-      <h1>Comments</h1>
-    </div>
+    <>
+      {comments.length > 0 && (
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+          <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+            {comments.length}
+            {comments.length > 1 ? " replies" : " reply"} Comments
+          </h3>
+          {comments.map((comment) => (
+            <div
+              className="mb-4 border-b border-gray-100 pb-4"
+              key={comment.createdAt}
+            >
+              <p className="mb-4">
+                <span className="font-semibold">{comment.name}</span> On{" "}
+                {moment(comment.createdAt).format("MMM DD, YYYY")}
+              </p>
+              <p className="whitespace-pre-line text-gray-600 w-full">
+                {parse(comment.comment)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
--- services/index.js
+++ services/index.js
@@ -133,3 +133,17 @@ export const submitComment = async (obj) => {
   });
   return result.json();
 };
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        name
+        createdAt
+        comment
+      }
+    }
+  `;
+  const response = await request(graphqlAPI, query, { slug });
+  return response.comments;
+};
